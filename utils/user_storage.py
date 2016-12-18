5b37ef01f95597b679ee063b1164cdbc9e7965ac
Yury Kuptsou <yurasb@tut.by>
2016-12-18 22:02:45 +0300

    1. splitting valid and invalid user data 2. splitting get_user_by method by valid and invalid user

--- utils/user_storage.py
+++ utils/user_storage.py
@@ -4,12 +4,20 @@ from tinydb import TinyDB
 from data.user import User
 
 
+VALID = 'valid_user_data'
+INVALID = 'invalid_user_data'
+
 user_db = TinyDB(
     '../data/user_db.json',
-    default_table='valid_user_data'
+    default_table=VALID
 )
 
 
-def get_user_by(index):
-    user_data = user_db.get(eid=index)
+def get_valid_user_by(index):
+    user_data = user_db.table(name=VALID).get(eid=index)
+    return User(**user_data)
+
+
+def get_invalid_user_by(index):
+    user_data = user_db.table(name=INVALID).get(eid=index)
     return User(**user_data)
