26aada001f70e6339af80b5ccf96cd40cc845991
Yury Kuptsou <yurasb@tut.by>
2016-12-20 19:25:45 +0300

    1. split user fixture by valid and invalid user 2. parametrised both fixtures 3. added test_fail_registration and method for it inside RegistrationPage 4. change data for invalid user - leaving one required field empty for each set

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-
+import webium.settings
 from selenium import webdriver
 from webium.driver import close_driver
-import webium.settings
 
-from data.user import User
 from pages.drag_and_drop import DraganddropPage
 from pages.registration import RegistrationPage
+from utils.user_storage import get_valid_user_by, get_invalid_user_by
 
 
 @pytest.fixture(scope='session')
@@ -16,9 +15,14 @@ def reg_page():
     return RegistrationPage()
 
 
-@pytest.fixture(scope='session')
-def user():
-    return User()
+@pytest.fixture(scope='session', params=range(4))
+def valid_user(request):
+    return get_valid_user_by(request.param)
+
+
+@pytest.fixture(scope='session', params=range(7))
+def invalid_user(request):
+    return get_invalid_user_by(request.param)
 
 
 @pytest.fixture(scope='session')
@@ -28,21 +32,21 @@ def dnd_page():
 
 @pytest.fixture(scope='session', autouse=True)
 def setup_webdriver(request):
-    def sauce_lab():
-        desired_cap = {
-            'platform': "Windows 7",
-            'browserName': "firefox",
-            'version': "47",
-        }
-
-        driver = webdriver.Remote(
-            command_executor=('http://Sausage:Party'
-                              '@ondemand.saucelabs.com:80/wd/hub'),
-            desired_capabilities=desired_cap)
-
-        return driver
-
-    webium.settings.driver_class = sauce_lab
+    # def sauce_lab():
+    #     desired_cap = {
+    #         'platform': "Windows 7",
+    #         'browserName': "firefox",
+    #         'version': "47",
+    #     }
+    #
+    #     driver = webdriver.Remote(
+    #         command_executor=('http://Sausage:Party'
+    #                           '@ondemand.saucelabs.com:80/wd/hub'),
+    #         desired_capabilities=desired_cap)
+    #
+    #     return driver
+    #
+    # webium.settings.driver_class = sauce_lab
 
     def fin():
         close_driver()
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-def test_register_user(reg_page, user):
+def test_register_user(reg_page, valid_user):
     reg_page.open()
-    reg_page.register_user(user)
+    reg_page.register_user(valid_user)
     assert reg_page.is_registration_success()
