447df99f534926ec4e303240432bd398484d9e14
Yury Kuptsou <yurasb@tut.by>
2016-12-22 21:44:15 +0300

    1. merge both tests for registration of user (valid and invalid) into one file 2. deleting test_fail_registration file

--- tests/test_fail_registration.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-def test_register_user(reg_page, invalid_user):
-    reg_page.open()
-    reg_page.register_user(invalid_user)
-    assert reg_page.is_registration_failed()
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -5,3 +5,9 @@ def test_register_user(reg_page, valid_user):
     reg_page.open()
     reg_page.register_user(valid_user)
     assert reg_page.is_registration_success()
+
+
+def test_not_register_user(reg_page, invalid_user):
+    reg_page.open()
+    reg_page.register_user(invalid_user)
+    assert reg_page.is_registration_failed()
