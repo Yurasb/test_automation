f36e6f8fc884ba5296bd4a3029c33e7455402aae
Yury Kuptsou <yurasb@tut.by>
2016-12-27 12:56:00 +0300

    Reformatting

--- tests/test_drag_and_drop.py
+++ tests/test_drag_and_drop.py
@@ -3,7 +3,7 @@
 
 def test_drag_and_drop(dnd_page):
     dnd_page.open()
-    dnd_page.drag_and_drop(dnd_page.drag_div,
-                           dnd_page.drop_div
-                           )
+    dnd_page.drag_and_drop(
+        dnd_page.drag_div, dnd_page.drop_div
+    )
     assert dnd_page.is_drag_and_drop_success()
