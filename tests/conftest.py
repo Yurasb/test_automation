db2f4561592427c45c75345af31cfef59bdafe8a
Yury Kuptsou <yurasb@tut.by>
2016-12-29 21:45:06 +0300

    1. adding fixture to made screenshot on test failure 2. integrate it with Allure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import pytest
+import allure
 import webium.settings
 from selenium import webdriver
-from webium.driver import close_driver
+from webium.driver import close_driver, get_driver_no_init
+from allure.constants import AttachmentType
 
 from pages.drag_and_drop import DraganddropPage
 from pages.home_page import HomePage
 from pages.registration import RegistrationPage
 from utils.user_storage import get_valid_user_by, get_invalid_user_by
+
+
+@pytest.mark.tryfirst
+def pytest_runtest_makereport(item, call, __multicall__):
+    rep = __multicall__.execute()
+    setattr(item, 'rep_' + rep.when, rep)
+    return rep
 
 
 @pytest.fixture(scope='session')
@@ -34,6 +43,19 @@ def dnd_page():
 @pytest.fixture(scope='session')
 def home_page():
     return HomePage()
+
+
+@pytest.fixture(autouse=True)
+def screenshot_on_fail(request):
+    def fin():
+        if request.node.rep_call.failed:
+            allure.attach(
+                request.node.name,
+                get_driver_no_init().get_screenshot_as_png(),
+                type=AttachmentType.PNG
+            )
+
+    request.addfinalizer(fin)
 
 
 @pytest.fixture(scope='session', autouse=True)
